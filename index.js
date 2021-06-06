080a766eb3a31ec0869fbc79dd48f42489373f83
Uzma <Ujma1810170@akgec.ac.in>
2021-06-06 21:52:23 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function updateMap() {
+    //fetching the api's
+    console.log("Updating map with realtime data")
+    fetch('./data.json')
+        .then(response => response.json()
+            .then(rsp => {
+                console.log(rsp.data)
+                //accessing the ids and important events
+                rsp.data.forEach(element => {
+                    latitude = element.latitude;
+                    longitude = element.longitude;
+    
+                    cases = element.infected;
+                    if (cases>255){
+                        color = "rgb(255, 0, 0)";
+                    }
+    
+                    else{
+                        color = `rgb(${cases}, 0, 0)`;
+                    }
+    
+                    // Mark on the map
+                    new mapboxgl.Marker({
+                        draggable: false,
+                        color: color
+                    }).setLngLat([longitude, latitude])
+                    .addTo(map); 
+                });
+
+            })
+
+        )
+}
+
+let interval = 20000;
+setInterval( updateMap, interval); 
